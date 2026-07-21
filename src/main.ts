0048876210016f4f537a58c75a9417ab1aad7178
Katve <kati@katvef.fi>
2026-07-21 18:01:28 +0300

    Removed handlebars safe helper as I found a native method

--- src/main.ts
+++ src/main.ts
@@ -188,7 +188,6 @@ Hooks.once("init", () => {
 	});
 
 	Handlebars.registerHelper("objectLength", (obj: object) => Object.keys(obj ?? {}).length);
-	Handlebars.registerHelper("safe", (s: string) => new Handlebars.SafeString(s));
 
 	// @ts-expect-error ForgeVTT exclusive variable
 	game.isForge = !!(globalThis.ForgeVTT && ForgeVTT.usingTheForge);
