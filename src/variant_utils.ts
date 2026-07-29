368be26db3ba3fbb0dd6854da7740bbd802ea338
Katve <kati@katvef.fi>
2026-07-29 16:19:32 +0300

    Automatically create default variant when adding variants

--- src/variant_utils.ts
+++ src/variant_utils.ts
@@ -5,10 +5,10 @@ export async function addVariantPopup(scene: Scene) {
 	if (scene.getFlag(MODULE_NAME, "variants.Default") === undefined) {
 		addVariant(scene, "Default");
 		scene.setFlag(MODULE_NAME, "active", "Default");
-	} else {
-		const { variantName } = await foundry.applications.api.DialogV2.input({
-			window: { title: "Create Variant" },
-			content: `
+	}
+	const { variantName } = await foundry.applications.api.DialogV2.input({
+		window: { title: "Create Variant" },
+		content: `
 			<div class="form-group">
 				<form>
 					<label>Name</label>
@@ -18,9 +18,8 @@ export async function addVariantPopup(scene: Scene) {
 				</form>
 			</div>
 			`,
-		});
-		addVariant(scene, variantName as string);
-	}
+	});
+	addVariant(scene, variantName as string);
 }
 
 export function pickVariant(scene: Scene) {
